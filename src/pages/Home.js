c272daa100bf7e08ec7ff5880bd39dfcadf87f90
Fatima Ezzahra <felemenoun@gmail.com>
2022-07-06 23:43:04 +0100

    Fix reponsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
 	return (
 	<main className='w-full h-screen flex flex-col justify-center'>
-			<div className='flex flex-col sm:flex-row h-2/3 sm:space-x-5 space-y-5'>
+			<div className='flex flex-col items-center sm:items-start sm:flex-row h-2/3 sm:space-x-5 space-y-5'>
 				<div className='flex flex-col w-1/2 items-end space-x-5'>
 					<div className='bg-zinc-900 uppercase'>
 						<h1 className='text-4xl sm:text-7xl lg:text-8xl font-extrabold text-white text-right'>Insights.</h1>
